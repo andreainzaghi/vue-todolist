c4722335eddfe48822536c267c0218ca05cd7951
Andrea <andrea3aprile99@gmail.com>
2021-03-26 16:45:04 +0100

    n

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var app = new Vue(
+{
+  el: "#todo-list",
+    data: {
+    newItem: "",
+    lista: ['Comprare vino', 'Comprare latte', 'Comprare mirtilli']
+  },
+  methods: {
+    deleteItem: function(itemindex) {
+    this.lista.splice(itemIndex, 1);
+    },
+    addItem: function() {
+      if (this.newItem !== "") {
+      this.lista.push(this.newItem);
+      this.newItem = "";
+      }
+}
+}
+});
